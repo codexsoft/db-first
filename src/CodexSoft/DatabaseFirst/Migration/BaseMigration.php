0b25e857de47be7bc32e67d37ee6c9cd3e506a94
Dmitriy V Kozubskiy <kozubskiyd@vezubr.ru>
2019-11-11 06:26:32 +0700

    improved migrations sql parser (complex statements can be wrapped between --@statement and --@/statement tokens)

--- src/CodexSoft/DatabaseFirst/Migration/BaseMigration.php
+++ src/CodexSoft/DatabaseFirst/Migration/BaseMigration.php
@@ -2,9 +2,11 @@
 
 namespace CodexSoft\DatabaseFirst\Migration;
 
+use CodexSoft\Code\Helpers\Arrays;
 use CodexSoft\Code\Helpers\Classes;
 use Doctrine\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
+use function CodexSoft\Code\str;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
@@ -12,50 +14,90 @@ use Doctrine\DBAL\Schema\Schema;
 abstract class BaseMigration extends AbstractMigration
 {
 
+    private const SQL_STATEMENTS_AUTODETECT = 1;
+    private const SQL_STATEMENTS_MANUAL = 2;
+
+    protected const SQL_MANUAL_STATEMENT_TOKEN_OPEN = '--@statement';
+    protected const SQL_MANUAL_STATEMENT_TOKEN_CLOSE = '--@/statement';
+
     abstract public static function getContainingDirectory(): string;
 
-    /**
-     * @return string[]
-     * @throws \RuntimeException
-     */
     protected function getSqlStatements(): array
     {
-        $simpleStatements = $this->getSqlStatementsFromFileWithSimpleSyntaxOrFail();
-        $complexStatements = $this->getSqlStatementsFromFileWithComplexSyntax();
-        return array_merge($simpleStatements,$complexStatements);
-    }
-
-    /**
-     * @return string[]
-     * @throws \RuntimeException
-     */
-    protected function getSqlStatementsFromFileWithSimpleSyntaxOrFail(): array
-    {
         $migrationSqlFile = static::getContainingDirectory().'/'.Classes::short(static::class).'.sql';
-        if ( !file_exists($migrationSqlFile) ) {
-            throw new \RuntimeException( 'Migration file does not exists! '.$migrationSqlFile );
+        if (!\file_exists($migrationSqlFile)) {
+            throw new \RuntimeException('Migration file does not exists! '.$migrationSqlFile);
         }
 
-        // to allow multiple statemets via PDO, we use parser
-        return SqlParser::parseFile($migrationSqlFile);
-    }
+        $auto_detect_line_endings_prev_value = \ini_get('auto_detect_line_endings');
+        \ini_set('auto_detect_line_endings', true);
 
-    /**
-     * @return string[]
-     * @throws \RuntimeException
-     */
-    protected function getSqlStatementsFromFileWithComplexSyntax(): array
-    {
-        // some complex statements, like routines (functions) should not be parsed, for that cases
-        // we use R postfix in another sql file, statements separated by --@@@
-        $migrationSqlFile = static::getContainingDirectory().'/'.Classes::short(static::class).'R.sql';
-        if ( file_exists($migrationSqlFile) ) {
-            $contents = file_get_contents($migrationSqlFile);
-            return preg_split('/\r\n\-\-\@\@\@|\r\-\-\@\@\@|\n\-\-\@\@\@/', $contents);
-            //return explode(PHP_EOL.'--@@@'.PHP_EOL,$contents);
+        $migrationSqlFileHandler = \fopen($migrationSqlFile, 'rb');
+        if (!$migrationSqlFileHandler) {
+            \ini_set('auto_detect_line_endings', $auto_detect_line_endings_prev_value);
+            throw new \RuntimeException('Failed to open migration file '.$migrationSqlFile);
         }
-        return [];
 
+        $buffer = '';
+        $statements = [];
+        $sqlBufferMode = self::SQL_STATEMENTS_AUTODETECT;
+
+        while (($sqlLine = fgets($migrationSqlFileHandler, 4096)) !== false) {
+
+            if ($sqlLine === '') {
+                continue;
+            }
+
+            $trimmedSqlLine = str($sqlLine)->trimLeft();
+
+            if ($trimmedSqlLine->startsWith(static::SQL_MANUAL_STATEMENT_TOKEN_OPEN)) {
+
+                if ($buffer) {
+                    if ($sqlBufferMode === self::SQL_STATEMENTS_AUTODETECT) {
+                        Arrays::push($statements, ...SqlParser::parseString($buffer));
+                    } else {
+                        $statements[] = $buffer;
+                    }
+                    $buffer = '';
+                }
+
+                $sqlBufferMode = self::SQL_STATEMENTS_MANUAL;
+                continue;
+            }
+
+            if ($trimmedSqlLine->startsWith(static::SQL_MANUAL_STATEMENT_TOKEN_CLOSE)) {
+                if ($buffer && ($sqlBufferMode === self::SQL_STATEMENTS_MANUAL)) {
+                    $statements[] = $buffer;
+                    $buffer = '';
+                }
+
+                $sqlBufferMode = self::SQL_STATEMENTS_AUTODETECT;
+                continue;
+            }
+
+            if ($trimmedSqlLine->startsWith('--')) {
+                continue;
+            }
+
+            $buffer .= $sqlLine;
+        }
+
+        if ($buffer) {
+            if ($sqlBufferMode === self::SQL_STATEMENTS_AUTODETECT) {
+                Arrays::push($statements, ...SqlParser::parseString($buffer));
+            } else {
+                $statements[] = $buffer;
+            }
+        }
+
+        if (!feof($migrationSqlFileHandler)) {
+            throw new \RuntimeException('Migration file probably has problems with reading EOF! '.$migrationSqlFile);
+        }
+
+        \fclose($migrationSqlFileHandler);
+
+        \ini_set('auto_detect_line_endings', $auto_detect_line_endings_prev_value);
+        return $statements;
     }
 
     /**
