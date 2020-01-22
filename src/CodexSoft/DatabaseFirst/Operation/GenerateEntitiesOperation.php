f9f4d68038677a4a39a74c3bfee86d72400c0fa2
Dmitriy V Kozubskiy <kozubsky@inbox.ru>
2020-01-22 09:35:55 +0700

    stub for extractId

--- src/CodexSoft/DatabaseFirst/Operation/GenerateEntitiesOperation.php
+++ src/CodexSoft/DatabaseFirst/Operation/GenerateEntitiesOperation.php
@@ -491,6 +491,58 @@ class GenerateEntitiesOperation extends Operation
 
     }
 
+    protected function generateIdExtractors(ClassMetadataInfo $metadata): string
+    {
+        $lines = [];
+        [
+            '    /**',
+            '     * Выдаст ID, если передать ID, иначе вытащит id из сущности через getId()',
+            '     *',
+            '     * В некоторых случаях, чтобы не делать лишние запросы в БД, имея на руках ID',
+            '     * или сущность, можно пользоваться этим методом.',
+            '     *',
+            '     * @param int|string|static $idOrEntity',
+            '     *',
+            '     * @return int',
+            '     * @throws \RuntimeException',
+            '     */',
+            '    public static function extractId($idOrEntity): int',
+            '    {',
+            '        $id = static::extractIdOrNull($idOrEntity);',
+            '        if ($id === null) {',
+            '            throw new \RuntimeException("ID \'$idOrEntity\' is not entity nor numeric value");',
+            '        }',
+            '        return $id;',
+            '    }',
+            '    /**',
+            '     * todo: проверить что число >0 ?',
+            '     * @param int|string|static $idOrEntity',
+            '     *',
+            '     * @return int|null',
+            '     */',
+            '    public static function extractIdOrNull($idOrEntity): ?int',
+            '    {',
+            '        if (\is_string($idOrEntity) && \is_numeric($idOrEntity)) {',
+            '            return (int) $idOrEntity;',
+            '        }',
+            '        if (\is_int($idOrEntity)) {',
+            '            return $idOrEntity;',
+            '        }',
+            '        if ($idOrEntity instanceof static) {',
+            '            if (!\method_exists(\'getId\', $idOrEntity)) {',
+            '                throw new \RuntimeException(\'Entity of class \'.static::class.\' has NOT getId method!\');',
+            '            }',
+            '            if ($idOrEntity->getId() === null) {',
+            '                throw new \RuntimeException(\'Entity of class \'.static::class.\' has not ID yet!\');',
+            '            }',
+            '            return $idOrEntity->getId();',
+            '        }',
+            '        return null;',
+            '    }',
+        ];
+        return implode("\n".TAB, $lines);
+    }
+
     protected function generateSqlFieldNameHelpers(ClassMetadataInfo $metadata): string
     {
         $lines = ['','// to avoid human mistakes when using SQL table and columns names...',''];
