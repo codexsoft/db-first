5fe52b87d293dec1c588a2d9e6f1c2608d88f149
Dmitriy V Kozubskiy <kozubsky@inbox.ru>
2020-01-22 09:35:14 +0700

    RepoStaticAccessTrait became more documented

--- src/CodexSoft/DatabaseFirst/Orm/RepoStaticAccessTrait.php
+++ src/CodexSoft/DatabaseFirst/Orm/RepoStaticAccessTrait.php
@@ -6,6 +6,21 @@ use CodexSoft\Code\Classes\Classes;
 use Doctrine\ORM\EntityManagerInterface;
 
 /**
+ * Some helpers for access repository directly from entity class, for example:
+ *
+ * Product::repo() => ProductRepository
+ * Product::byId() => Product
+ * Product::byIdOrNull() => ?Product
+ * Product::byIds() => Product[]
+ * Product::randomId() => int
+ * Product::randomEntity() => Product
+ *
+ * All these methods expect ?EntityManagerInterface $em parameter, OR statically configured
+ * entity-repo map (via KnownEntityManagerContainer::setEntityManager($em)) and
+ * (new DoctrineOrmSchema)->setKnownEntityManagerContainerClass(KnownEntityManagerContainer::class)
+ *
+ * KnownEntityManagerContainer can be child (neccessary when multiple orm schemas and/or entity managers are used).
+ *
  * todo: previous implementation was expected single entity manager per domain,
  * so in repo(), byId() and other methods $em argument was absent, and it was handy.
  * should not we have 2 traits for both cases?
@@ -13,25 +28,28 @@ use Doctrine\ORM\EntityManagerInterface;
 trait RepoStaticAccessTrait
 {
     use KnownEntityManagerTrait;
+    use EntityExtractIdTrait;
 
     /**
+     * Caution! If entityManager argument is omitted, knownEntityManager must be properly configured
      * @param EntityManagerInterface $em
      *
      * @return \Doctrine\ORM\EntityRepository
      */
-    public static function repo(EntityManagerInterface $em = null)
+    public static function repo(?EntityManagerInterface $em = null)
     {
         return static::knownEntityManager($em)->getRepository(static::class);
     }
 
     /**
+     * Caution! If entityManager argument is omitted, knownEntityManager must be properly configured
      * @param int|string|static $id
      *
      * @param EntityManagerInterface $em
      *
      * @return static
      */
-    public static function byId($id, EntityManagerInterface $em = null)
+    public static function byId($id, ?EntityManagerInterface $em = null)
     {
         $foundEntity = static::byIdOrNull($id, $em);
         if (!$foundEntity instanceof static) {
@@ -42,91 +60,44 @@ trait RepoStaticAccessTrait
     }
 
     /**
+     * Caution! If entityManager argument is omitted, knownEntityManager must be properly configured
      * @param int[] $ids
      *
      * @param EntityManagerInterface $em
      *
      * @return static[]
      */
-    public static function byIds(array $ids, EntityManagerInterface $em = null)
+    public static function byIds(array $ids, ?EntityManagerInterface $em = null)
     {
         $repository = static::repo($em);
         if (method_exists($repository,'getByIdsIndexedById')) {
             return $repository->getByIdsIndexedById($ids);
         }
+        // todo: what if id is not present?
         //logger()->notice('called '.static::class.'::byIds and repo method getByIdsIndexedById is not defined');
         return $repository->findBy([static::_id() => $ids]);
     }
 
     /**
+     * Caution! If entityManager argument is omitted, knownEntityManager must be properly configured
      * @param int|string|static $id
      *
      * @param EntityManagerInterface $em
      *
      * @return static|null
      */
-    public static function byIdOrNull($id, EntityManagerInterface $em = null)
+    public static function byIdOrNull($id, ?EntityManagerInterface $em = null)
     {
-
         if ($id instanceof static) {
             return $id;
         }
 
         /** @noinspection PhpIncompatibleReturnTypeInspection */
         return self::repo($em)->find((int) $id);
-
     }
 
     /**
-     * Выдаст ID, если передать ID
-     *
-     * В некоторых случаях, чтобы не делать лишние запросы в БД, имея на руках ID
-     * или сущность, можно пользоваться этим методом.
-     *
-     * todo: проверить что число >0 ?
-     *
-     * @param int|string|static $idOrEntity
-     *
-     * @return int
-     * @throws \RuntimeException
-     */
-    public static function extractId($idOrEntity): int
-    {
-        $id = static::extractIdOrNull($idOrEntity);
-        if ($id === null) {
-            throw new \RuntimeException("ID '$idOrEntity' is not entity nor numeric value");
-        }
-
-        return $id;
-    }
-
-    /**
-     * @param int|string|static $idOrEntity
-     *
-     * @return int|null
-     */
-    public static function extractIdOrNull($idOrEntity): ?int
-    {
-        if (\is_string($idOrEntity) && \is_numeric($idOrEntity)) {
-            return (int) $idOrEntity;
-        }
-
-        if (\is_int($idOrEntity)) {
-            return $idOrEntity;
-        }
-
-        if ($idOrEntity instanceof static) {
-            // todo: is entity always implements has getId()? Maybe check that it has getter?
-            if ($idOrEntity->getId() === null) {
-                throw new \RuntimeException('Entity of class '.static::class.' has not ID yet!');
-            }
-            return $idOrEntity->getId();
-        }
-
-        return null;
-    }
-
-    /**
+     * Caution! If entityManager argument is omitted, knownEntityManager must be properly configured
      * @param EntityManagerInterface|null $em
      *
      * @return int|null
@@ -134,7 +105,7 @@ trait RepoStaticAccessTrait
      *
      * todo: all models should have method _db_table_()
      */
-    public static function randomId(EntityManagerInterface $em = null): ?int
+    public static function randomId(?EntityManagerInterface $em = null): ?int
     {
         $table = static::_db_table_();
         /** @noinspection SqlResolve */
@@ -148,12 +119,13 @@ trait RepoStaticAccessTrait
     }
 
     /**
+     * Caution! If entityManager argument is omitted, knownEntityManager must be properly configured
      * @param EntityManagerInterface|null $em
      *
      * @return static|null
      * @throws \Doctrine\DBAL\DBALException
      */
-    public static function randomEntity(EntityManagerInterface $em = null): ?self
+    public static function randomEntity(?EntityManagerInterface $em = null): ?self
     {
         $id = static::randomId();
         return static::byId($id, $em);
