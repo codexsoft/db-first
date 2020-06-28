1dc1f643dbdb8a51b2fdc7c5ac44d3dc318b4dfc
Dmitriy V Kozubskiy <kozubsky@inbox.ru>
2020-06-29 05:17:14 +0700

    polyfill for migrations CLI app

--- bin/doctrine.migrate.php
+++ bin/doctrine.migrate.php
@@ -2,9 +2,6 @@
 
 use CodexSoft\Cli\Cli;
 use CodexSoft\DatabaseFirst\DoctrineOrmSchema;
-use Doctrine\DBAL\Migrations\Configuration\Configuration;
-use Doctrine\DBAL\Migrations\Tools\Console\ConsoleRunner;
-use Doctrine\DBAL\Migrations\Tools\Console\Helper\ConfigurationHelper;
 use Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper;
 use Symfony\Component\Console\Helper\HelperSet;
 use Symfony\Component\Console\Helper\QuestionHelper;
@@ -15,14 +12,29 @@ $ormSchema = DoctrineOrmSchema::getFromConfigFile($ormConfigFile);
 
 $connection = $ormSchema->getEntityManager()->getConnection();
 
-$migrationsConfig = new Configuration($connection);
+if (\class_exists('Doctrine\Migrations\Configuration\Configuration')) {
+    $migrationsConfig = new \Doctrine\Migrations\Configuration\Configuration($connection);
+} else {
+    $migrationsConfig = new \Doctrine\DBAL\Migrations\Configuration\Configuration($connection); // doctrine migrations 1.x
+}
+
 $migrationsConfig->setMigrationsNamespace($ormSchema->getNamespaceMigrations());
 $migrationsConfig->setMigrationsDirectory($ormSchema->getPathToMigrations());
+
+if (\class_exists('Doctrine\Migrations\Tools\Console\ConsoleRunner')) {
+    $configurationHelper = new \Doctrine\Migrations\Tools\Console\Helper\ConfigurationHelper($connection, $migrationsConfig);
+} else {
+    $configurationHelper = new \Doctrine\DBAL\Migrations\Tools\Console\Helper\ConfigurationHelper($connection, $migrationsConfig);
+}
 
 $helperSet = new HelperSet([
     'db' => new ConnectionHelper($connection),
     new QuestionHelper,
-    new ConfigurationHelper($connection, $migrationsConfig),
+    $configurationHelper,
 ]);
 
-ConsoleRunner::createApplication($helperSet)->run();
+if (\class_exists('Doctrine\Migrations\Tools\Console\ConsoleRunner')) {
+    \Doctrine\Migrations\Tools\Console\ConsoleRunner::createApplication($helperSet)->run();
+} else {
+    \Doctrine\DBAL\Migrations\Tools\Console\ConsoleRunner::createApplication($helperSet)->run(); // doctrine migrations 1.x
+}
