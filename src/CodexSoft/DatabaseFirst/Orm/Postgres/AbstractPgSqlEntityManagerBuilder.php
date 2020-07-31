1bc2b62bc6934a7103c148c7ebbad75567bc0954
Dmitriy V Kozubskiy <kozubsky@inbox.ru>
2020-07-31 19:58:45 +0700

    fix builder

--- src/CodexSoft/DatabaseFirst/Orm/Postgres/AbstractPgSqlEntityManagerBuilder.php
+++ src/CodexSoft/DatabaseFirst/Orm/Postgres/AbstractPgSqlEntityManagerBuilder.php
@@ -2,12 +2,11 @@
 
 namespace CodexSoft\DatabaseFirst\Orm\Postgres;
 
-use CodexSoft\DatabaseFirst\DoctrineOrmSchema;
 use CodexSoft\DatabaseFirst\Orm\DoctrineEntityLifecycleEventSubscriber;
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Configuration;
-use \MartinGeorgiev\Doctrine\DBAL\Types as MartinGeorgievTypes;
 use CodexSoft\DatabaseFirst\Orm\Postgres\Types\BigIntCastingToIntType;
 use Doctrine\Common\Cache\CacheProvider;
 use Doctrine\Common\Cache\VoidCache;
@@ -16,11 +15,12 @@ use Doctrine\DBAL\Types\Type;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Tools\Setup;
-use Ramsey\Uuid\Doctrine\UuidType;
 
 abstract class AbstractPgSqlEntityManagerBuilder
 {
-
+    /**
+     * @deprecated override via getDoctrineTypeMapping istead
+     */
     public const CUSTOM_DOMAINS = [
 
         // while reverse-engineering, this types were detected. in normal mode it still exists?
@@ -61,7 +61,7 @@ abstract class AbstractPgSqlEntityManagerBuilder
      */
     protected ?Connection $connection = null;
 
-    /** @var \Doctrine\Common\EventManager if no event manager provided, new one will be created */
+    /** @var EventManager if no event manager provided, new one will be created */
     protected ?EventManager $eventManager = null;
 
     public function __construct() {
@@ -96,24 +96,6 @@ abstract class AbstractPgSqlEntityManagerBuilder
         return [];
     }
 
-    public function tuneConfiguration(Configuration $config): void
-    {
-        $config->setCustomStringFunctions($this->getStringFunctions());
-        $config->setCustomNumericFunctions($this->getNumericFunctions());
-        $config->setMetadataDriverImpl(new \Doctrine\Persistence\Mapping\Driver\PHPDriver($this->getMappingDirectories()));
-
-        ///**
-        // * todo: this should be fixed someday
-        // * Somewhy \Doctrine\DBAL\Schema\AbstractSchemaManager::filterAssetNames() ignores
-        // * configured filterSchemaAssetsExpressionCallable
-        // */
-        //$config->setSchemaAssetsFilter(function($tableName) {
-        //    return false === DoctrineOrmSchema::tableShouldBeSkipped(
-        //        $tableName, $this->doctrineOrmSchema->skipTables
-        //    );
-        //});
-    }
-
     /**
      * @return EntityManagerInterface|EntityManager
      * @throws \Doctrine\DBAL\DBALException
@@ -121,8 +103,6 @@ abstract class AbstractPgSqlEntityManagerBuilder
      */
     public function build(): EntityManagerInterface
     {
-        static::addCustomTypes();
-
         if ($this->configuration instanceof Configuration) {
             $config = $this->configuration;
         } else {
@@ -149,32 +129,68 @@ abstract class AbstractPgSqlEntityManagerBuilder
             $eventManager = new EventManager();
         }
 
-        $this->tuneConfiguration($config);
-        $this->tuneEventManager($eventManager);
+        $config->setCustomStringFunctions($this->getStringFunctions());
+        $config->setCustomNumericFunctions($this->getNumericFunctions());
+        $config->setMetadataDriverImpl(new \Doctrine\Persistence\Mapping\Driver\PHPDriver($this->getMappingDirectories()));
+
+        ///**
+        // * todo: this should be fixed someday
+        // * Somewhy \Doctrine\DBAL\Schema\AbstractSchemaManager::filterAssetNames() ignores
+        // * configured filterSchemaAssetsExpressionCallable
+        // */
+        //$config->setSchemaAssetsFilter(function($tableName) {
+        //    return false === DoctrineOrmSchema::tableShouldBeSkipped(
+        //        $tableName, $this->doctrineOrmSchema->skipTables
+        //    );
+        //});
+
+        $eventManager->addEventSubscriber(new DoctrineEntityLifecycleEventSubscriber());
 
         $entityManager = EntityManager::create($connection, $config, $eventManager);
+
+        /**
+         * Registering types
+         */
+        $platform = $entityManager->getConnection()->getDatabasePlatform();
+        static::installTypes($platform);
+        foreach (static::getDoctrineTypeMapping() as $domainName => $domainType) {
+            $platform->registerDoctrineTypeMapping($domainName, $domainType);
+        }
+
+        $this->tuneConfiguration($config);
+        $this->tuneEventManager($eventManager);
         $this->tuneEntityManager($entityManager);
 
         return $entityManager;
     }
 
+    protected static function getDoctrineTypeMapping(): array
+    {
+        return static::CUSTOM_DOMAINS;
+    }
+
     /**
+     * Use this hook to modify EntityManager after creation
      * @param EntityManagerInterface $entityManager
-     *
-     * @throws \Doctrine\DBAL\DBALException
-     * @throws \Exception
      */
     public function tuneEntityManager(EntityManagerInterface $entityManager): void
     {
-        $platform = $entityManager->getConnection()->getDatabasePlatform();
+    }
 
-        if ($platform === null) {
-            throw new \Exception('Database platform is not detected!');
-        }
+    /**
+     * Use this hook to modify Configuration after creation
+     * @param Configuration $config
+     */
+    public function tuneConfiguration(Configuration $config): void
+    {
+    }
 
-        foreach( static::CUSTOM_DOMAINS as $domainName => $domainType ) {
-            $platform->registerDoctrineTypeMapping($domainName, $domainType);
-        }
+    /**
+     * Use this hook to modify EventManager after creation
+     * @param EventManager $eventManager
+     */
+    public function tuneEventManager(EventManager $eventManager): void
+    {
     }
 
     /**
@@ -197,49 +213,72 @@ abstract class AbstractPgSqlEntityManagerBuilder
         $platform->registerDoctrineTypeMapping($typeName, $typeName);
     }
 
-    public function tuneEventManager(EventManager $eventManager): void
+    /**
+     * @param AbstractPlatform $platform
+     * @param string $typeName
+     * @param string $typeClass
+     * @param bool $overrideIfExists
+     *
+     * @param bool $throwExceptionIfClassNotFound
+     *
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public static function installType(AbstractPlatform $platform, string $typeName, string $typeClass, bool $overrideIfExists = true, bool $throwExceptionIfClassNotFound = false): void
     {
-        $eventManager->addEventSubscriber(new DoctrineEntityLifecycleEventSubscriber);
+        if (!\class_exists($typeClass)) {
+            if ($throwExceptionIfClassNotFound) {
+                throw new \RuntimeException("Failed to install Doctrine type: $typeClass is not exist");
+            }
+            return;
+        }
+
+        if (!Type::hasType($typeName)) {
+            Type::addType($typeName, $typeClass);
+        } elseif ($overrideIfExists) {
+            Type::overrideType($typeName, $typeClass);
+        }
+
+        $platform->registerDoctrineTypeMapping($typeName, $typeName);
     }
 
     /**
+     * Most common/popular types
+     * @return array|string[]
+     */
+    protected static function defaultTypesToInstall(): array
+    {
+        return [
+            Types::BIGINT => BigIntCastingToIntType::class,
+            'smallint[]' => \MartinGeorgiev\Doctrine\DBAL\Types\SmallIntArray::class,
+            'integer[]' => \MartinGeorgiev\Doctrine\DBAL\Types\IntegerArray::class,
+            'bigint[]' => \MartinGeorgiev\Doctrine\DBAL\Types\BigIntArray::class,
+            'jsonb[]' => \MartinGeorgiev\Doctrine\DBAL\Types\JsonbArray::class,
+            'text[]' => \MartinGeorgiev\Doctrine\DBAL\Types\TextArray::class,
+            'varchar[]' => \MartinGeorgiev\Doctrine\DBAL\Types\TextArray::class,
+            \Ramsey\Uuid\Doctrine\UuidType::NAME => \Ramsey\Uuid\Doctrine\UuidType::class,
+        ];
+    }
+
+    /**
+     * @return array|string[]
+     */
+    protected static function getTypesToInstall(): array
+    {
+        return static::defaultTypesToInstall();
+    }
+
+    /**
+     * Because of Doctrine's global type mapping settings
+     *
+     * @param AbstractPlatform $platform
+     *
      * @throws \Doctrine\DBAL\DBALException
      */
-    public static function addCustomTypes(): void
+    protected static function installTypes(AbstractPlatform $platform): void
     {
-        $types = [
-            'smallint[]' => MartinGeorgievTypes\SmallIntArray::class,
-            'integer[]' => MartinGeorgievTypes\IntegerArray::class,
-            'bigint[]' => MartinGeorgievTypes\BigIntArray::class,
-            'jsonb[]' => MartinGeorgievTypes\JsonbArray::class,
-            'text[]' => MartinGeorgievTypes\TextArray::class,
-            'varchar[]'  => MartinGeorgievTypes\TextArray::class,
-        ];
-
-        foreach ($types as $typeName => $typeClass) {
-
-            if (\is_int($typeName)) {
-                // this will trigger PHP deprecated call non-static method statically
-                /** @var \MartinGeorgiev\Doctrine\DBAL\Types\BaseType $typeName */
-                $typeName = $typeClass::TYPE_NAME;
-                //$typeName = $typeClass::getName();
-            }
-
-            if (!Type::hasType($typeName)) {
-                Type::addType( $typeName, $typeClass );
-            } else {
-                Type::overrideType( $typeName, $typeClass );
-            }
-
+        foreach (static::getTypesToInstall() as $typeName => $typeClass) {
+            static::installType($platform, $typeName, $typeClass);
         }
-
-        if (!Type::hasType(UuidType::NAME)) {
-            Type::addType(UuidType::NAME, UuidType::class);
-        } else {
-            Type::overrideType(UuidType::NAME, UuidType::class);
-        }
-
-        Type::overrideType( Types::BIGINT, BigIntCastingToIntType::class );
     }
 
     /**
