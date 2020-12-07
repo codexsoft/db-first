cc01bd99f20420c65b57fa0461d132df9a1e9523
Dmitriy V Kozubskiy <kozubsky@inbox.ru>
2020-12-08 00:13:05 +0700

    cli methods.

--- bin/codexsoft.dbfirst.php
+++ bin/codexsoft.dbfirst.php
@@ -7,4 +7,9 @@ require_once __DIR__.'/findautoloader.php';
 //$input = new ArgvInput();
 //$configFile = $input->getFirstArgument();
 $configFile = (new ArgvInput())->getFirstArgument();
-DatabaseFirst::createApplication($configFile, __FILE__)->run();
+$cli = DatabaseFirst::createApplication($configFile, __FILE__);
+
+$args = $_SERVER['argv'];
+\array_shift($args);
+
+$cli->run(new ArgvInput($args));
